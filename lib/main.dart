5646be11bdb89918562bc68bc498748b99711f2b
hilmisyarif406 <hilmisyarif406@gmail.com>
2021-05-12 03:01:16 +0700

    CodeLab 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_app/detile_view.dart';
+import 'package:wisata_app/detail_screen.dart';
+import 'package:wisata_app/main_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: true,
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
